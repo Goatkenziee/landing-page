07618817ebda38c8565166af3f5bac134febbc32
Goatkenziee <mackquisition@gmail.com>
2026-05-31 22:49:14 -0400

    Initial commit of landing page project

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
-const HomePage: React.FC = () => {
+export default function Home() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen flex flex-col">
+    <main>
       <Hero />
       <Services />
       <Contact />
-      <Footer />
-    </div>
+    </main>
   );
-};
-
-export default HomePage;
+}
